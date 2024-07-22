8d5b48c2a9b20c7e8f7c0bf3ee5764c22911d943
Aadarsh <v16ash@gmail.com>
2024-07-22 15:00:25 +0530

    Adding Layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './components'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  return (
-    <></>
-  )
+	return (
+		<>
+            <Header />
+            <div>
+                <Course/>
+                <TimeTable/>
+                <Downloader/>
+            </div>
+            <Footer/>
+		</>
+	)
 }
 
 export default App
